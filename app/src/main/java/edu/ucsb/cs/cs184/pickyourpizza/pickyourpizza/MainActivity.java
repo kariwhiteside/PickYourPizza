367841e01a547434d8cc083d0e31301f7ea6ace1
Kari Whiteside <Kari@Karis-MacBook-Pro.local>
2017-11-29 22:20:11 -0800

    first commit

---
+++ app/src/main/java/edu/ucsb/cs/cs184/pickyourpizza/pickyourpizza/MainActivity.java
@@ -0,0 +1,13 @@
+package edu.ucsb.cs.cs184.pickyourpizza.pickyourpizza;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
