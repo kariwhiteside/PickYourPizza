1302d815195e0d520e5da339c4231a641e354a46
Joe Song <joesong@Joes-MacBook-Pro.local>
2017-12-04 19:48:28 -0800

    Finished cheese image adapter

--- app/src/main/java/edu/ucsb/cs/cs184/pickyourpizza/pickyourpizza/CheeseImageAdapter.java
+++ app/src/main/java/edu/ucsb/cs/cs184/pickyourpizza/pickyourpizza/CheeseImageAdapter.java
@@ -1,8 +1,81 @@
 package edu.ucsb.cs.cs184.pickyourpizza.pickyourpizza;
 
+import android.app.Activity;
+import android.content.Context;
+import android.graphics.Color;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
 /**
  * Created by joesong on 12/4/17.
  */
 
 public class CheeseImageAdapter {
+    private Context mContext;
+    private ArrayList<Integer> selectedPositions = new ArrayList<>();
+
+    public CheeseImageAdapter(Context c) {
+        mContext = c;
+    }
+
+    public int getCount() {
+        return mThumbIds.length;
+    }
+
+    public Object getItem(int position) {
+        return null;
+    }
+
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    public void setSelectedPositions(int position){
+        if(selectedPositions.contains(position)){
+            selectedPositions.remove(Integer.valueOf(position));
+        }
+        else {
+            selectedPositions.add(position);
+        }
+    }
+
+    // create a new ImageView for each item referenced by the Adapter
+    public View getView(int position, View convertView, ViewGroup parent) {
+        View myView;
+
+        LayoutInflater li = ((Activity) mContext).getLayoutInflater();
+        myView = li.inflate(R.layout.grid_item, null);
+        ImageView imageView = (ImageView) myView.findViewById(R.id.grid_img);
+        TextView txt = (TextView) myView.findViewById(R.id.grid_txt);
+
+        imageView.setImageResource(mThumbIds[position]);
+        txt.setText(mThumbNames[position]);
+
+        if(selectedPositions.contains(position)) {
+            imageView.setBackgroundColor(Color.BLUE);
+        } else {
+            imageView.setBackgroundColor(Color.TRANSPARENT);
+        }
+        return myView;
+    }
+
+    // references to our images
+    private Integer[] mThumbIds = {
+            R.drawable.extramozzarella, R.drawable.fetacheese,
+            R.drawable.gorgonzolacheese, R.drawable.parmesancheese,
+            R.drawable.ricottacheese, R.drawable.romanocheese,
+            R.drawable.vegancheese
+    };
+
+    private String[] mThumbNames = {
+            "Extra Mozzarella", "Feta Cheese",
+            "Gorgonzola Cheese", "Parmesan Cheese",
+            "Ricotta Cheese", "Romano Cheese",
+            "Vegan Cheese"
+    };
 }
